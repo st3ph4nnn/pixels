53343575621dfbc6bdc971f686abc1435255f3eb
Stefan Arteni <stephanarteni@gmail.com>
2022-04-16 09:45:00 -0700

    initial upload

---
+++ files/utils.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func get_random(min, max int) int32 {
+	return int32(rand.Intn(max-min+1) + min)
+}
+
+func get_random_float(min, max float32) float32 {
+	return float32(float32(rand.Intn(int(max-min+1))) + min)
+}
+
+func get_random_color() rl.Color {
+	col := get_random(1, 6)
+
+	switch col {
+	case 1:
+		return rl.Black
+	case 2:
+		return rl.Red
+	case 3:
+		return rl.Blue
+	case 4:
+		return rl.Magenta
+	case 5:
+		return rl.DarkGreen
+	case 6:
+		return rl.Purple
+	}
+
+	return rl.Black
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module pixel
+
+go 1.18
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20220324192015-efabb001d2f1
+
+require (
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	modernc.org/mathutil v1.4.1 // indirect
+)
